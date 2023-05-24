3e8143faae8ec1c3d1eaee17a25bafe29d6d461c
koju <kojubivek@gmail.com>
2023-05-25 03:45:15 +1000

    stripe integrated

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import express from "express";
 import { dbConnect } from "./src/dbconfig/dbCongif.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
+app.use(cors());
 dbConnect();
 import userRouter from "./src/routers/userRouter.js";
 import authRouter from "./src/routers/auth.js";
 import productRouter from "./src/routers/productRouter.js";
 import cartRouter from "./src/routers/cartRouter.js";
 import orderRouter from "./src/routers/orderRouter.js";
+import stripeRouter from "./src/routers/stripeRouter.js";
 
 app.use("/api/v1/auth", authRouter);
 
@@ -20,6 +24,7 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/cart", cartRouter);
 app.use("/api/v1/order", orderRouter);
+app.use("/api/v1/checkout", stripeRouter);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
--- src/routers/stripeRouter.js
+++ src/routers/stripeRouter.js
@@ -1,3 +1,34 @@
 import express from "express";
 import stripe from "stripe";
 const router = express.Router();
+
+const stripeFun = stripe(process.env.STRIPE_KEY);
+
+router.get("/payment", (req, res, next) => {
+  res.json({
+    status: "success",
+    message: "payment",
+  });
+});
+router.post("/payment", (req, res, next) => {
+  stripeFun.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "aud",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        next(error);
+      } else {
+        res.json({
+          status: "success",
+          message: "payment succesfull",
+          stripeRes,
+        });
+      }
+    }
+  );
+});
+
+export default router;
